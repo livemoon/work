d7ae80e8de46d22d0cb68a672bffde14f34976be
livemoon <deadsun@mwj.(none)>
2011-11-10 15:14:58 +0800

    add add_user.pl

---
+++ add_user.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+use Expect;
+
+
+my $host = shift;
+my $name = shift;
+my $sudo;
+my $keyfile = shift;
+open my $FH, '<', $keyfile or die "Cannot open file\n";
+my $key = <$FH>;
+chomp $key;
+my $exp = Expect->new;
+$exp = Expect->spawn("ssh $host");
+$exp->expect(5,
+             [ qr/connecting/ => sub { my $exp = shift;
+                                $exp->send("yes\n");
+                                exp_continue;} ],
+             [ qr/password/i => sub { my $exp = shift;
+                                $exp->send("Mko09ijn\n");} ],
+             );
+$exp->send("uname -a\n") if ($exp->expect(undef,'$'));
+my $read = $exp->before();
+$sudo="wheel\n" if $read =~ /el/;
+$sudo="admin\n" if $read =~ /Ubuntu/;
+chomp $sudo;
+$exp->send("sudo useradd -G $sudo -m -k /etc/skel -s /bin/bash $name\n") if ($exp->expect(undef,'$'));
+$exp->send("sudo su - $name -c \"mkdir ~/.ssh\"\n") if ($exp->expect(undef,'$'));
+$exp->send("sudo su - $name -c \"echo $key > ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys\"\n") if ($exp->expect(undef,'$'));
+$exp->send("exit\n") if ($exp->expect(undef,'$'));
+$exp->soft_close();
